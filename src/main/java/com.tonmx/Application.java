f31fc1c8dbbe0ff7fc7818602e5494211f4349e4
elln <elln.bu@tonmx.com>
2020-11-03 09:29:09 +0800

    init

---
+++ src/main/java/com.tonmx/Application.java
@@ -0,0 +1,37 @@
+package com.tonmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Locale;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAspectJAutoProxy
+@EnableTransactionManagement
+@EnableConfigurationProperties
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    @Bean
+    public Locale locale() {
+        return new Locale("zh","CN");
+    }
+
+
+}
